db41e298fffac5fa7ed81c0d5b395d82ce8396a9
ianlevesque <ian@ianlevesque.org>
2009-03-27 10:22:18 -0400

    C version builds, doesn't work.

--- lib/lzma.rb
+++ lib/lzma.rb
@@ -1,19 +1,23 @@
 # Ruby/JRuby LZMA
 # This code, like the LZMA SDK, is public domain.
 
-require 'java'
-require 'lzma_java.jar'
+unless RUBY_PLATFORM =~ /java/
+  require 'lzma_ruby'
+else
+  require 'java'
+  require 'lzma_java.jar'
 
-class LZMA
-  def initialize
-    @lzma = com.ephemeronindustries.lzma.LZMA.new
-  end
+  class LZMA
+    def initialize
+      @lzma = com.ephemeronindustries.lzma.LZMA.new
+    end
   
-  def compress(data)
-    String.from_java_bytes @lzma.compress(data.to_java_bytes)
-  end
+    def compress(data)
+      String.from_java_bytes @lzma.compress(data.to_java_bytes)
+    end
   
-  def decompress(data)
-    String.from_java_bytes @lzma.decompress(data.to_java_bytes)
+    def decompress(data)
+      String.from_java_bytes @lzma.decompress(data.to_java_bytes)
+    end
   end
 end
--- test/test_lzma.rb
+++ test/test_lzma.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby -I../ext
 #!/opt/local/bin/jruby -I../java
 # Ruby/JRuby LZMA
 # This code, like the LZMA SDK, is public domain.
@@ -15,6 +16,8 @@ class TestLzma < Test::Unit::TestCase
   end
   
   def test_lzma_both_ways
-    assert_equal("leroy was here", @lzma.decompress(@lzma.compress("leroy was here")))
+    compressed = @lzma.compress("leroy was here")
+    assert_not_nil(compressed)
+    assert_equal("leroy was here", @lzma.decompress(compressed))
   end
 end
